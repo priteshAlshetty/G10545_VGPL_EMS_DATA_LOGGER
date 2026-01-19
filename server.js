7c04a1fdebc341f6ed6aa4493529009e19bf83e8
priteshAlshetty <alshettypritesh4@gmail.com>
2026-01-19 16:10:37 +0530

    flag added to prevent overlapping call of scheduler

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./src/app');
 const { startScheduler, stopScheduler } = require('./src/scheduler/scheduler');
 const PORT = 3300;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     // ✅ SCHEDULER STARTS HERE
     startScheduler();
@@ -13,5 +13,7 @@ process.on('SIGINT', shutdown);
 function shutdown() {
     console.log('Shutting down service...');
     stopScheduler();
-    server.close(() => process.exit(0));
+    server.close(() =>
+        console.log('data logger service is shut down. Exiting process........') ||
+        process.exit(0));
 }
